885c411a9c86f4912c45bd42795a064a878f1c3c
hugmar <hugmar@lliurex-ALDA-CE>
2021-12-02 18:28:42 +0100

    min max temp en 1

--- comitpush/src/comitpush/Comitpush.java
+++ comitpush/src/comitpush/Comitpush.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package comitpush;
-
+import java.util.Scanner;
 /**
  *
  * @author hugmar
@@ -14,8 +14,23 @@ public class Comitpush {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int temp;
         System.out.println("Bienvenido/a");
         // TODO code application logic here
+        System.out.println("Introduce una temperatura y te dire si es positiva o negativa:");
+        temp=input.nextInt();
+        if (temp>0){
+            System.out.println("La temperatura es positiva.");
+        }
+        else{
+            if(temp<0){
+                System.out.println("Latemperatura es negativa.");
+            }
+            else{
+                System.out.println("La temperatura es 0, por lo tanto no es ni negativa ni positiva.");
+            }
+        }
     }
     
 }
